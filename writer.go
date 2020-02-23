1ed74f0bfcd8860bf59e2f84661d37e58f8a1a80
Dmitry Yu Okunev <xaionaro@dx.center>
2020-02-23 15:38:15 +0000

    Fix Write()

--- writer.go
+++ writer.go
@@ -26,16 +26,19 @@ func NewWriter(storage []byte) *Writer {
 // Write writes `b` into the `Storage`.
 //
 // If the `Storage` is full then returns io.EOF as `err`.
-// On partial write returns `nil` as `err`.
+// On partial write returns `io.ErrShortWrite` as `err`.
 func (w *Writer) Write(b []byte) (n int, err error) {
 	if w.CurrentPosition >= uint(len(w.Storage)) {
 		return 0, io.EOF
 	}
-	copy(w.Storage, b)
+	copy(w.Storage[w.CurrentPosition:], b)
 	n = min(
 		int(uint(len(w.Storage))-w.CurrentPosition),
 		len(b),
 	)
+	if n < len(b) {
+		err = io.ErrShortWrite
+	}
 	w.CurrentPosition += uint(n)
 	return
 }
